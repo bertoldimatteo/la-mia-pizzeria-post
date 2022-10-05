14c4b20978dc7bb03e1e6891d8fa719543c200af
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-10-05 14:06:42 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using la_mia_pizzeria_post.Models;
+
+namespace la_mia_pizzeria_post.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public int Price { get; set; }
+
+        public Pizza(string name, string description,string photo, int price)
+        {
+            Name = name;
+            Description = description;
+            Photo = photo;
+            Price = price;
+        }
+    }
+}
